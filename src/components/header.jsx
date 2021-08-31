3ff8525a0e96c9ff11df138e0c2563af06beb397
Bruno Costa <b.brunocosta@outlook.com>
2021-08-31 11:53:05 -0300

    feat: add CEP api request

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import useScroll from '../hooks/useScroll'
+import { useState } from 'react'
 const HeaderLeft = () => {
   const { scrollTo, aboutRef, solutionsRef } = useScroll()
   
@@ -19,21 +20,50 @@ const HeaderLeft = () => {
   )
 }
 const HeaderRight = () => {
+  const [cep, setCep] = useState('')
+  const [ address, setAddress ] = useState(null)
+
+  const handleSeatch = e => {
+    if(isVaidCepString(cep))
+      fetch(`https://viacep.com.br/ws/${cep}/json`)
+      .then(parseJson)
+      .then(setAddress)
+      .catch(alertError)
+    else
+      alert('CEP invalido! digite um CEP com esse formato xxxxxxxx ou xxxxx-xxx')    
+  }
+  const parseJson = httpResponse => httpResponse.json()
+  const alertError = err => alert('Enderço não encontrado! O CEP está correto?')
+  const isVaidCepString = cep => /[0-9]{5}-*[\d]{3}/.test(cep)
+  const handleInputChange = e => setCep(e.target.value)
+  const handleAdressClick = e => setAddress(null)
+
+  const  Search = !address&&
+                    <div className="search">
+                      <input type="text" minLength="7" maxLength="9" name="search" placeholder="Digite seu CEP" value={cep} onChange={handleInputChange}/>
+                      <button onClick={handleSeatch} title="Buscar">
+                        <img src="/assets/svgs/search.svg" alt="" />
+                      </button> 
+                    </div>
+
+  const  Address = address&&
+                    <div className="address">
+                      <h4 title="Clique para Alterar" onClick={handleAdressClick}>
+                        {address.logradouro}, {address.bairro}, {address.localidade}-{address.uf}
+                      </h4>
+                    </div>
   return (
     <div className="header-right">
-    <div className="search">
-      <input type="text" placeholder="Buscar"/>
-      <button onClick={e=>alert('Esta ação ainda não implementada!')}>
-        <img src="/assets/svgs/search.svg" alt="" />
-      </button>
-    </div>
+      { Search }
+      { Address }
     <button onClick={e=>alert('Esta ação ainda não implementada!')}> Login </button>
     <button onClick={e=>alert('Esta ação ainda não implementada!')}>
-      <img src="/assets/svgs/shopping-cart.svg" alt="" />
+      <img src="/assets/svgs/shopping-cart.svg" alt="" title="carrinho de compras"/>
     </button>
   </div>
   )
 }
+
 const Header = () => {
   return (
       <header className="header">
